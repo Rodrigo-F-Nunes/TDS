74f87b538151509dd81b32c02712a86c558914c2
Rodrigo-F-Nunes <odeioestargripado@gmail.com>
2025-08-18 12:13:10 -0300

    no need for this

--- enemyInvaders.java
+++ enemyInvaders.java
@@ -1,14 +1,2 @@
-import javax.swing.*;
 
-public class enemyInvaders extends JPanel {
-
-    int panelWidth = 15;
-    int PanelHeight = 15;
-    int yAxisVelocity = 1;
-
-    enemyInvaders(){
-        this.setBackground(Color.black);
-    }
-
-}
 
